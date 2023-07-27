dbfd57f105e787933b9d38ccb59598ae854d0b99
Milad Nekofar <milad@nekofar.com>
2023-07-27 09:42:47 +0330

    refactor: remove redundant declaration in project generator
    Removed the explicit String return type on the packageName function within the TauriCliProjectGenerator.kt file. This change was made to follow the Kotlin coding convention which encourages to avoid redundant specification of the return type when it can be inferred by the compiler.

--- src/main/kotlin/com/nekofar/milad/intellij/tauri/cli/TauriCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/tauri/cli/TauriCliProjectGenerator.kt
@@ -21,7 +21,7 @@ class TauriCliProjectGenerator : NpmPackageProjectGenerator() {
 
     override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
 
-    override fun packageName(): String = packageName
+    override fun packageName() = packageName
 
     override fun presentablePackageName() = message("tauri.project.generator.presentable.package.name")
 
